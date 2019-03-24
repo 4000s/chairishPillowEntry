46fa703080b2e1eedb651ade8cb5ad9d7bec98b2
HEARI-NOTEBOOK <hasanemreari@gmail.com>
2019-03-24 15:22:34 +0300

    minor changes.

--- ChairishPillowEntry/pillow_config.py
+++ ChairishPillowEntry/pillow_config.py
@@ -1,8 +1,8 @@
 # Hasan Emre ARI
 file = '51663-51732.xlsx'
 dir_input = 'C:\\Users\\hasan\\Desktop\\50x50\\'
-startRow = 0
-endRow =  1 #if endRow equal '-1', this meaning is go on end of the list
+startRow = 1
+endRow =  25 #if endRow equal '-1', this meaning is go on end of the list
 
 
 # Okan YILDIRIM
